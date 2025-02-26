87c74da5e7a7adcdfc875af0765c196766c76012
Eran <eran.hasarali.2001@gmail.com>
2025-02-26 16:41:49 +0530

    remove some lines

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,3 @@
 import "./style.css";
 
-const obj = { name: "Alice", age: 25 };
-const jsonString = JSON.stringify(obj);
-console.log(jsonString); 
-
-const jsObj = JSON.parse(jsonString)
-console.log(jsObj);
 
